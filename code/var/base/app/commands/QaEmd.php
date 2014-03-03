803f9d5cfc97f560865ab705276c2befa8d605ca
Andrew Yasinsky <andrew@yasinsky.com>
2014-03-03 10:56:55 -0800

    -typo

--- code/var/base/app/commands/QaEmd.php
+++ code/var/base/app/commands/QaEmd.php
@@ -181,7 +181,7 @@ class QaEmd extends Command {
             $this->log_qa_db($invoice, "Payment amount is greater than Invoice due amount!",'netsuite');
         }
 
-        if(($invoice->InvoiceseStatus_Code == 'PAID') && (int($invoice->InvoiceTotal) > int($invoice->PaymentTotal)))
+        if(($invoice->InvoicesStatus_Code == 'PAID') && (int($invoice->InvoiceTotal) > int($invoice->PaymentTotal)))
         {
             $this->log_qa_db($invoice, "Invoice closed with outstanding balance. Please use write off codes" , 'warning');
         }
