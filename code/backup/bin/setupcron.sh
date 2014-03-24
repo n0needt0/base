9d191100bf74f57229d7c970ea27d36bd6136af1
Andrew Yasinsky <andrew@yasinsky.com>
2014-03-24 12:29:52 -0700

    -changed cron

--- code/backup/bin/setupcron.sh
+++ code/backup/bin/setupcron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMMAND="/var/backup/bin/db.sh && curl http://www.cronrat.com/r/7n9GvvYu/BASEDB_backup "; 
+COMMAND="/var/backup/bin/db.sh && curl http://www.cronrat.com/r/7n9GvvYu/BASEDB_backup?activeon=1111100&toutc=25200"; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 12,15,18 * * 1,2,3,4,5 bash $COMMAND"; 
@@ -10,7 +10,7 @@ echo "$ENTRY" >> $FILE2;
 crontab $FILE2;
 rm $FILE $FILE2
 
-COMMAND="bash /var/backup/bin/_logmonitors/monitorlog.sh && curl http://www.cronrat.com/r/7n9GvvYu/basemonitorlog?activeon=1111100"; 
+COMMAND="bash /var/backup/bin/_logmonitors/monitorlog.sh && curl http://www.cronrat.com/r/7n9GvvYu/basemonitorlog?activeon=1111100&toutc=25200"; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
@@ -20,7 +20,7 @@ echo "$ENTRY" >> $FILE2;
 crontab $FILE2;
 rm $FILE $FILE2
 
-COMMAND="php /var/www/base/artisan command:qaemd --env=production && curl http://www.cronrat.com/r/7n9GvvYu/qaemd?activeon=1111100"; 
+COMMAND="php /var/www/base/artisan command:qaemd --env=production && curl http://www.cronrat.com/r/7n9GvvYu/qaemd?activeon=1111100&toutc=25200"; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
@@ -30,7 +30,7 @@ echo "$ENTRY" >> $FILE2;
 crontab $FILE2;
 rm $FILE $FILE2
 
-COMMAND="php /var/www/base/artisan command:moveemdbilltrac all --env=production && curl http://www.cronrat.com/r/7n9GvvYu/moveemdbilltrac?activeon=1111100"; 
+COMMAND="php /var/www/base/artisan command:moveemdbilltrac all --env=production && curl http://www.cronrat.com/r/7n9GvvYu/moveemdbilltrac?activeon=1111100&toutc=25200"; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
@@ -40,7 +40,7 @@ echo "$ENTRY" >> $FILE2;
 crontab $FILE2;
 rm $FILE $FILE2
 
-COMMAND="php /var/www/base/artisan command:moveemdnetsuite all --env=production && curl http://www.cronrat.com/r/7n9GvvYu/moveemdnetuite?activeon=1111100"; 
+COMMAND="php /var/www/base/artisan command:moveemdnetsuite all --env=production && curl http://www.cronrat.com/r/7n9GvvYu/moveemdnetuite?activeon=1111100&toutc=25200"; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
@@ -50,7 +50,7 @@ echo "$ENTRY" >> $FILE2;
 crontab $FILE2;
 rm $FILE $FILE2
 
-COMMAND="php /var/www/base/artisan command:dayemd --env=production && curl http://www.cronrat.com/r/7n9GvvYu/dayemd?activeon=1111100"; 
+COMMAND="php /var/www/base/artisan command:dayemd --env=production && curl http://www.cronrat.com/r/7n9GvvYu/dayemd?activeon=1111100&toutc=25200"; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
