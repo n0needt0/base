99ec2c40939f1f839eb27ffeacce4fe2306b37a3
Andrew Yasinsky <andrew@yasinsky.com>
2014-04-23 13:55:27 -0700

    crontab

--- code/backup/bin/setupcron.sh
+++ code/backup/bin/setupcron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMMAND="/var/backup/bin/db.sh && curl \"http://www.cronrat.com/r/7n9GvvYu/BASEDB_backup?crontab=0+12%2C15%2C18+%2A+%2A+1%2C2%2C3%2C4%2C5&toutc=-7\""; 
+COMMAND="/var/backup/bin/db.sh && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=BASEDB_backup\" --data-urlencode \"crontab=0 12,15,18 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 12,15,18 * * 1,2,3,4,5 bash $COMMAND"; 
@@ -10,7 +10,7 @@ echo "$ENTRY" >> $FILE2;
 crontab $FILE2;
 rm $FILE $FILE2
 
-COMMAND="bash /var/backup/bin/_logmonitors/monitorlog.sh && curl \"http://www.cronrat.com/r/7n9GvvYu/basemonitorlog?crontab=0+1+%2A+%2A+1%2C2%2C3%2C4%2C5&toutc=-7\""; 
+COMMAND="bash /var/backup/bin/_logmonitors/monitorlog.sh && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=basemonitorlog\" --data-urlencode \"crontab=0 1 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
@@ -20,7 +20,7 @@ echo "$ENTRY" >> $FILE2;
 crontab $FILE2;
 rm $FILE $FILE2
 
-COMMAND="php /var/www/base/artisan command:qaemd --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu/qaemd?crontab=0+1+%2A+%2A+1%2C2%2C3%2C4%2C5&toutc=-7\""; 
+COMMAND="php /var/www/base/artisan command:qaemd --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=qaemd\" --data-urlencode \"crontab=0 1 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
@@ -30,7 +30,7 @@ echo "$ENTRY" >> $FILE2;
 crontab $FILE2;
 rm $FILE $FILE2
 
-COMMAND="php /var/www/base/artisan command:moveemdbilltrac all --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu/moveemdbilltrac?crontab=0+1+%2A+%2A+1%2C2%2C3%2C4%2C5&toutc=-7\""; 
+COMMAND="php /var/www/base/artisan command:moveemdbilltrac all --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=moveemdbilltrac\" --data-urlencode \"crontab=0 1 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
@@ -40,7 +40,7 @@ echo "$ENTRY" >> $FILE2;
 crontab $FILE2;
 rm $FILE $FILE2
 
-COMMAND="php /var/www/base/artisan command:moveemdnetsuite all --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu/moveemdnetuite?crontab=0+1+%2A+%2A+1%2C2%2C3%2C4%2C5&toutc=-7\""; 
+COMMAND="php /var/www/base/artisan command:moveemdnetsuite all --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=moveemdnetuite\" --data-urlencode \"crontab=0 1 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
@@ -50,7 +50,7 @@ echo "$ENTRY" >> $FILE2;
 crontab $FILE2;
 rm $FILE $FILE2
 
-COMMAND="php /var/www/base/artisan command:dayemd --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu/dayemd?crontab=0+1+%2A+%2A+1%2C2%2C3%2C4%2C5&toutc=-7\""; 
+COMMAND="php /var/www/base/artisan command:dayemd --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=dayemd\" --data-urlencode \"crontab=0 1 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
