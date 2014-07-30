e38ca3a15afcb21fcf03c9dd5e79a348ec7c5f41
Andrew Yasinsky <andrew@yasinsky.com>
2014-07-30 09:48:25 -0700

    -fixed crontab a bit

--- code/backup/bin/setupcron.sh
+++ code/backup/bin/setupcron.sh
@@ -1,71 +1,33 @@
 #!/bin/bash
+FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 
 COMMAND="/var/backup/bin/db.sh && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=BASEDB_backup\" --data-urlencode \"crontab=0 12,15,18 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
-FILE="/tmp/$(basename $0).$RANDOM.txt"; 
-FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 12,15,18 * * 1,2,3,4,5 bash $COMMAND"; 
-crontab -l > $FILE 2> /dev/null; 
-cat $FILE | grep -v "$COMMAND" > $FILE2; 
-echo "$ENTRY" >> $FILE2; 
-crontab $FILE2;
-rm $FILE $FILE2
+echo "$ENTRY" >> $FILE; 
 
 COMMAND="bash /var/backup/bin/_logmonitors/monitorlog.sh && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=basemonitorlog\" --data-urlencode \"crontab=0 1 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
-FILE="/tmp/$(basename $0).$RANDOM.txt"; 
-FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
-crontab -l > $FILE 2> /dev/null; 
-cat $FILE | grep -v "$COMMAND" > $FILE2; 
-echo "$ENTRY" >> $FILE2; 
-crontab $FILE2;
-rm $FILE $FILE2
+echo "$ENTRY" >> $FILE; 
 
 COMMAND="php /var/www/base/artisan command:qaemd --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=qaemd\" --data-urlencode \"crontab=0 1 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
-FILE="/tmp/$(basename $0).$RANDOM.txt"; 
-FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
-crontab -l > $FILE 2> /dev/null; 
-cat $FILE | grep -v "$COMMAND" > $FILE2; 
-echo "$ENTRY" >> $FILE2; 
-crontab $FILE2;
-rm $FILE $FILE2
+echo "$ENTRY" >> $FILE; 
 
 COMMAND="php /var/www/base/artisan command:moveemdbilltrac all --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=moveemdbilltrac\" --data-urlencode \"crontab=0 1 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
-FILE="/tmp/$(basename $0).$RANDOM.txt"; 
-FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
-crontab -l > $FILE 2> /dev/null; 
-cat $FILE | grep -v "$COMMAND" > $FILE2; 
-echo "$ENTRY" >> $FILE2; 
-crontab $FILE2;
-rm $FILE $FILE2
+echo "$ENTRY" >> $FILE; 
 
 COMMAND="php /var/www/base/artisan command:moveemdnetsuite all --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=moveemdnetuite\" --data-urlencode \"crontab=0 1 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
-FILE="/tmp/$(basename $0).$RANDOM.txt"; 
-FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
-crontab -l > $FILE 2> /dev/null; 
-cat $FILE | grep -v "$COMMAND" > $FILE2; 
-echo "$ENTRY" >> $FILE2; 
-crontab $FILE2;
-rm $FILE $FILE2
+echo "$ENTRY" >> $FILE; 
 
 COMMAND="php /var/www/base/artisan command:dayemd --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=dayemd\" --data-urlencode \"crontab=0 1 * * 1,2,3,4,5\" --data-urlencode \"toutc=-7\""; 
-FILE="/tmp/$(basename $0).$RANDOM.txt"; 
-FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 1,2,3,4,5 $COMMAND"; 
-crontab -l > $FILE 2> /dev/null; 
-cat $FILE | grep -v "$COMMAND" > $FILE2; 
-echo "$ENTRY" >> $FILE2; 
-crontab $FILE2;
-rm $FILE $FILE2
+echo "$ENTRY" >> $FILE; 
 
 COMMAND="php /var/www/base/artisan command:dayees --env=production && curl \"http://www.cronrat.com/r/7n9GvvYu\" --data-urlencode \"rat=dayes\" --data-urlencode \"crontab=0 1 * * 3\" --data-urlencode \"toutc=-7\""; 
-FILE="/tmp/$(basename $0).$RANDOM.txt"; 
-FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
 ENTRY="0 1 * * 3 $COMMAND"; 
-crontab -l > $FILE 2> /dev/null; 
-cat $FILE | grep -v "$COMMAND" > $FILE2; 
-echo "$ENTRY" >> $FILE2; 
-crontab $FILE2;
-rm $FILE $FILE2
+echo "$ENTRY" >> $FILE; 
+
+crontab $FILE;
+rm $FILE
