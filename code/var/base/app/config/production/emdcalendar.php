2158c7012320f8e7eee1cae165497fe47f7e84c2
Andrew Yasinsky <andrew@yasinsky.com>
2014-08-04 16:24:35 -0700

    -config production

---
+++ code/var/base/app/config/production/emdcalendar.php
@@ -0,0 +1,27 @@
+<?php
+return  array(
+                "dryrun"=>false,  //run but not email
+                "test"=>false,  //test setup
+                "providerf"=>"", //filter to provider
+                "force"=>false,  //disregard status and run anyway
+                "force_proxy"=>false, //run through proxies of give provider
+                "providers"=>array(
+                                "Ahluwlia, Navneet S."=>array('email'=>"ahluwlia.navneet.calendar@helppain.net", 'proxy'=>array()),
+                                "Azevedo, Michael"=>array('email'=>"azevedo.michael.calendar@helppain.net", 'proxy'=>array()),
+                                "Brose, William"=>array('email'=>"brose.william.calendar@helppain.net", 'proxy'=>array(
+                                                'Gaeta/Brose Consult'=>"Gaeta, Raymond R.",
+                                                'Gaeta/Brose f/u'=>"Gaeta, Raymond R.",
+                                                'Schonwald/Brose Consult'=>"Schonwald, Gabriel",
+                                                'Schonwald/Brose f/u'=>"Schonwald, Gabriel",
+                                                'Azevedo/Brose Consult'=>"Azevedo, Michael",
+                                                'Azevedo/Brose f/u'=>"Azevedo, Michael",
+                                                'Ahluwalia/Brose Consult'=>"Ahluwlia, Navneet S.",
+                                                'Ahluwalia/Brose f/u'=>"Ahluwlia, Navneet S."
+                                ) ),
+                                "Gaeta, Raymond R."=>array('email'=>"gaeta.raymond.calendar@helppain.net", 'proxy'=>array(
+                                                'Azevedo/Gaeta Consult'=> "Azevedo, Michael",
+                                                'Azevedo/Gaeta f/u'=>"Azevedo, Michael"
+                                )),
+                                "Schonwald, Gabriel"=>array('email'=>"schonwald.gabriel.calendar@helppain.net", 'proxy'=>array()),
+                ),
+);
--- code/var/base/app/libraries/Lasdorf/EmdApi/ApiEmdCalendar.php
+++ code/var/base/app/libraries/Lasdorf/EmdApi/ApiEmdCalendar.php
@@ -48,6 +48,10 @@ Class ApiEmdCalendar extends EmdBase{
     static public function get_calendar()
     {
         $providers = Config::get('emdcalendar.providers');
+        $providerf = Config::get('emdcalendar.providerf');
+        $force = Config::get('emdcalendar.force');
+        $force_proxy = Config::get('emdcalendar.force_proxy');
+        $test = Config::get('emdcalendar.test');
 
         $result = array();
         $res = DB::connection('emds')->table("VIEW_API_Appointment")->where('startyyyymmdd', '>=', date('Ymd', time()))->where('patient_file','<>','')->where('patient_id','<>','0000000000')->whereIn('resource',array_keys($providers))->orderBy('startyyyymmdd', 'asc')->get();
@@ -55,21 +59,86 @@ Class ApiEmdCalendar extends EmdBase{
 
         foreach($res as $r)
         {
-            if(!empty($providers[$r->resource]))
+            usleep(100);
+
+            if($test && !empty($providerf))
             {
+                if( $r->resource == $providerf)
+                {
+                    $filter = true;
+                }
+                else
+                {
+                    $filter = false;
+                }
+            }
+            else
+            {
+                $filter = true;
+            }
+
+            if($filter && !empty($providers[$r->resource]))
+            {
+
+                if($test && $force_proxy)
+                {
+                    $proxies = array_keys($providers[$r->resource]['proxy']);
+                    $randproxy = rand(0, count($proxies)-1);
+                    \Log::info('force proxy to ' . $proxies[$randproxy]);
+                    $r->appointment_type = $proxies[$randproxy];
+                }
+
                 //create hash
                 $appthash = md5(serialize($r)) . "\n";
                 //check if key exists for given "APPT:ID"
                 $key = "APPT:" . $r->appointment_id;
                 $lookup = self::lookup($key);
 
-                if(empty($lookup) || trim((string)$lookup) != trim((string)$appthash))
+                //modify some time stamps nto unix types
+                $r->startf = strtotime($r->startf);
+                $r->endf = strtotime($r->endf);
+
+                //run exceptions
+                //check if status == 5 //rollback all dates to 3 years 365 * 24 * 60 * 60
+                if((string)$r->status == '5')
+                {
+                    $r->startf = $r->startf - (3 * 365 * 24 *60 * 60);
+                    $r->endf = $r->endf - (3 * 365 * 24 *60 * 60);
+                    $r->startf = $r->startf - (3 * 365 * 24 *60 * 60);
+                }
+
+                if( $force || empty($lookup) || trim((string)$lookup) != trim((string)$appthash))
                 {
                     //save appontment key ttl to appointment + 1 day
-                    if( self::set_appointment($r,$providers[$r->resource]) )
+                    //SEE I IT IS PROXY
+
+                    if(!empty($providers[$r->resource]['proxy']) && !empty($providers[$r->resource]['proxy'][$r->appointment_type]))
+                    {
+                        //this is proxy calendar
+                        //this a small hack to make appointment d to be bit different
+                        $r->appointment_id = $r->appointment_id . md5($providers[$r->resource]['email']);
+
+                        $email = $providers[$providers[$r->resource]['proxy'][$r->appointment_type]]['email'];
+                        \Log::info("PROXY " . $r->appointment_type . " proxy provider from " . $providers[$r->resource]['email'] . " to " . $providers[$providers[$r->resource]['proxy'][$r->appointment_type]]['email'] );
+                    }
+                    else
+                    {
+                        $email = $providers[$r->resource]['email'];
+                    }
+
+
+                    if( self::set_appointment($r,$email) )
                     {
                         $i++;
-                        self::store($key, $appthash, strtotime($r->startf) - time() + 60*60*24 );
+                        if((string)$r->status == '5')
+                        {
+                            \Log::info("CANCEL appointment on " . date('m-d', $r->startf) . ' ' .  $r->patient_name  . " (" . $r->dob . ") " . $r->appointment_type );
+                        }
+                        else
+                        {
+                            \Log::info("SET appointment on " . date('m-d', $r->startf) . ' ' .  $r->patient_name  . " (" . $r->dob . ") " . $r->appointment_type );
+                        }
+                        self::store($key, $appthash, 5*60*60*24 );
                     }
                 }
                 else
@@ -89,17 +158,35 @@ Class ApiEmdCalendar extends EmdBase{
         $message.="CALSCALE:GREGORIAN\n";
         $message.="METHOD:REQUEST\n";
         $message.="BEGIN:VEVENT\n";
-        $message.="DTSTART:" . date('Ymd\THis', strtotime($r->startf)) . "\n";
-        $message.="DTEND:" . date('Ymd\THis', strtotime($r->endf)) . "\n";
+
+        //roll back on cancel 3 years
+        if((string)$r->status == '5')
+        {
+
+            $message.="DTSTART:" . date('Ymd\THis', ($r->startf - 3*365*24*60*60)) . "\n";
+        }
+        else
+        {
+            $message.="DTSTART:" . date('Ymd\THis', $r->startf) . "\n";
+        }
+        if((string)$r->status == '5')
+        {
+            $message.="DTEND:" . date('Ymd\THis', ($r->endf - 3*365*24*60*60)) . "\n";
+        }
+        else
+        {
+            $message.="DTEND:" . date('Ymd\THis', $r->endf) . "\n";
+        }
+
         $message.="DTSTAMP:" . date('Ymd\THis', time()) . "\n";
         $message.="ORGANIZER;CN=Emd:mailto:emd@helppain.net\n";
         $message.="UID:" . $r->appointment_id . "\n";
         $message.="ATTENDEE;PARTSTAT=NEEDS-ACTION;RSVP= FALSE;CN=Emd:mailto:emd@helppain.net\n";
-        $message.="DESCRIPTION:" . $r->appointment_type . " | " . $r->patient_name . "\n";
+        $message.="DESCRIPTION:" . $r->patient_name  . " (" . $r->dob . ") " . $r->appointment_type . "\n";
         $message.="LOCATION: " . $r->facility . "\n";
         $message.="SEQUENCE:0\n";
         $message.="STATUS:CONFIRMED\n";
-        $message.="SUMMARY:" . $r->appointment_type . " | " . $r->patient_name . " | " . $r->facility . "\n";
+        $message.="SUMMARY:" . $r->notes . "\n";
         $message.="TRANSP:OPAQUE\n";
         $message.="END:VEVENT\n";
         $message.="END:VCALENDAR";
@@ -113,8 +200,21 @@ Class ApiEmdCalendar extends EmdBase{
         $headers .= "Content-Transfer-Encoding: 7bit";
 
         /*mail content , attaching the ics detail in the mail as content*/
-        $subject = $r->appointment_type . " with " . $r->patient_name;
+        if((string)$r->status == '5')
+        {
+            $subject = 'CANCEL';
+        }
+        else
+        {
+            $subject = $r->patient_name  . " ( " . $r->dob . " ) " . $r->appointment_type;
+        }
+
         $subject = html_entity_decode($subject, ENT_QUOTES, 'UTF-8');
+
+        if(Config::get('emdcalendar.dryrun'))
+        {
+            return true;
+        }
 
         if( $res = mail($email, $subject, $message, $headers) )
         {
