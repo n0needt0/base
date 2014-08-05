f6909d1038e451ff8df7f881520f3da865734383
Andrew Yasinsky <andrew@yasinsky.com>
2014-08-04 17:21:23 -0700

    trf

--- code/var/base/app/libraries/Lasdorf/EmdApi/ApiEmdCalendar.php
+++ code/var/base/app/libraries/Lasdorf/EmdApi/ApiEmdCalendar.php
@@ -105,7 +105,6 @@ Class ApiEmdCalendar extends EmdBase{
                     $r->startf = $r->startf - (3 * 365 * 24 *60 * 60);
                     $r->endf = $r->endf - (3 * 365 * 24 *60 * 60);
                     $r->startf = $r->startf - (3 * 365 * 24 *60 * 60);
-                    $force = true;
                 }
 
                 if( $force || empty($lookup) || trim((string)$lookup) != trim((string)$appthash))
