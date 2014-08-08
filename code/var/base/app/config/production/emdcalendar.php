2c140b0550905a9a97e704433a3781d2cd967005
Andrew Yasinsky <andrew@yasinsky.com>
2014-08-08 11:54:34 -0700

    azevedo

--- code/var/base/app/config/production/emdcalendar.php
+++ code/var/base/app/config/production/emdcalendar.php
@@ -4,7 +4,7 @@ return  array(
                 "force"=>false,  //disregard status and run anyway
                 "providers"=>array(
                                 //"Ahluwalia, Navneet S."=>array('email'=>"ahluwalia.navneet.calendar@helppain.net"),
-                                //"Azevedo, Michael"=>array('email'=>"azevedo.michael.cal@helppain.net"),
+                                "Azevedo, Michael"=>array('email'=>"azevedo.michael.cal@helppain.net"),
                                 //"Brose, William"=>array('email'=>"brose.william.cal@helppain.net"),
                                 //"Gaeta, Raymond R."=>array('email'=>"gaeta.raymond.cal@helppain.net"),
                                 "Schonwald, Gabriel"=>array('email'=>"schonwald.gabriel.cal@helppain.net")
